762fe8d4f037e5a0bd2c426b1bf2b8e41aaf9b1e
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2023-12-27 03:49:32 +0200

    [+] feat create chat storyboard

---
+++ Sources/iAtableViewPOC/ChatVC2.swift
@@ -0,0 +1,29 @@
+//
+//  ChatVC2.swift
+//  
+//
+//  Created by iSlam AbdelAziz on 27/12/2023.
+//
+
+import UIKit
+
+class ChatVC2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
