6f8cd86df45c332f89f743c243623564bf52aded
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2023-12-27 15:53:22 +0200

    [+] fix: loading bundles fix

--- Sources/iAtableViewPOC/iAtableViewPOC.swift
+++ Sources/iAtableViewPOC/iAtableViewPOC.swift
@@ -4,18 +4,15 @@ import SwiftUI
 public
 class IAChatManager {
     
-    var chatView: ChatVC2?
+    var chatView: IAChatVC?
+    var chatView2: ChatVC2?
     var chatSwiftUIView: ChatSwiftUI?
 
     public
     init(chatTableViewCellNib: UINib, identifier: String) {
-//        let bundle = Bundle(for: IAChatVC.self)
-//        let id = bundle.bundleIdentifier
-//        let bundle2 = Bundle(for: ChatVC2.self)
-//        let bundle3 = Bundle(for: IAChatVC.self)
-////        chatView = IAChatVC(nibName: "IAChatVC", bundle: Bundle(identifier: "com.IA.iAtableViewPOC"))
-//        chatView = UIStoryboard(name: "ChatStoryboard", bundle: nil).instantiateViewController(identifier: "ChatVC2")
-//        chatView?.configureChatView(chatTableViewCellNib: chatTableViewCellNib, identifier: identifier)
+        chatView = IAChatVC(nibName: "IAChatVC", bundle: .module)
+        chatView = UIStoryboard(name: "ChatStoryboard", bundle: .module).instantiateViewController(identifier: "ChatVC2")
+        chatView?.configureChatView(chatTableViewCellNib: chatTableViewCellNib, identifier: identifier)
     }
     
     public
@@ -29,7 +26,7 @@ class IAChatManager {
     }
     
     public
-    func getChatSwiftUIView() -> (any ChatSwiftUIType)? {
+    func getChatSwiftUIView() -> (any View)? {
         return chatSwiftUIView
     }
     
