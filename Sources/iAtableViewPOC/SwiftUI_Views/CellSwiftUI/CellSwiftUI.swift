d301bff3f0514b2c03b5b2bed38d354fe74800bb
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2023-12-27 14:22:01 +0200

    [+] feat: add swiftUI views

---
+++ Sources/iAtableViewPOC/SwiftUI_Views/CellSwiftUI/CellSwiftUI.swift
@@ -0,0 +1,23 @@
+//
+//  CellSwiftUI.swift
+//
+//
+//  Created by iSlam AbdelAziz on 27/12/2023.
+//
+
+import SwiftUI
+
+struct CellSwiftUI: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text ("Cell,")
+            }
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#Preview {
+    CellSwiftUI()
+}
--- Sources/iAtableViewPOC/iAtableViewPOC.swift
+++ Sources/iAtableViewPOC/iAtableViewPOC.swift
@@ -1,23 +1,36 @@
 import UIKit
+import SwiftUI
 
 public
 class IAChatManager {
     
     var chatView: ChatVC2?
-    
+    var chatSwiftUIView: ChatSwiftUI?
+
     public
     init(chatTableViewCellNib: UINib, identifier: String) {
-        let bundle = Bundle(for: IAChatVC.self)
-        let id = bundle.bundleIdentifier
-        let bundle2 = Bundle(for: ChatVC2.self)
-        let bundle3 = Bundle(for: IAChatVC.self)
-//        chatView = IAChatVC(nibName: "IAChatVC", bundle: Bundle(identifier: "com.IA.iAtableViewPOC"))
-        chatView = UIStoryboard(name: "ChatStoryboard", bundle: nil).instantiateViewController(identifier: "ChatVC2")
-        chatView?.configureChatView(chatTableViewCellNib: chatTableViewCellNib, identifier: identifier)
+//        let bundle = Bundle(for: IAChatVC.self)
+//        let id = bundle.bundleIdentifier
+//        let bundle2 = Bundle(for: ChatVC2.self)
+//        let bundle3 = Bundle(for: IAChatVC.self)
+////        chatView = IAChatVC(nibName: "IAChatVC", bundle: Bundle(identifier: "com.IA.iAtableViewPOC"))
+//        chatView = UIStoryboard(name: "ChatStoryboard", bundle: nil).instantiateViewController(identifier: "ChatVC2")
+//        chatView?.configureChatView(chatTableViewCellNib: chatTableViewCellNib, identifier: identifier)
+    }
+    
+    public
+    init(cell: any View) {
+        
     }
     
     public
     func getChatView() -> UIViewController? {
         return chatView
     }
+    
+    public
+    func getChatView() -> (any ChatSwiftUIType)? {
+        return chatSwiftUIView
+    }
+    
 }
