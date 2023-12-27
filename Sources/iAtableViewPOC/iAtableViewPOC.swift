9c97265057363e6758a01a1463b95f696636f252
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2023-12-27 16:59:23 +0200

    [-] perf: remove swiftUI files [+] fix: UIKint bundle

--- Sources/iAtableViewPOC/iAtableViewPOC.swift
+++ Sources/iAtableViewPOC/iAtableViewPOC.swift
@@ -5,29 +5,17 @@ public
 class IAChatManager {
     
     var chatView: IAChatVC?
-    var chatView2: ChatVC2?
-    var chatSwiftUIView: ChatSwiftUI?
+//    var chatView2: ChatVC2?
 
     public
     init(chatTableViewCellNib: UINib, identifier: String) {
         chatView = IAChatVC(nibName: "IAChatVC", bundle: .module)
-        chatView = UIStoryboard(name: "ChatStoryboard", bundle: .module).instantiateViewController(identifier: "ChatVC2")
+//        chatView = UIStoryboard(name: "ChatStoryboard", bundle: .module).instantiateViewController(identifier: "ChatVC2")
         chatView?.configureChatView(chatTableViewCellNib: chatTableViewCellNib, identifier: identifier)
     }
-    
-    public
-    init(cell: any View) {
         
-    }
-    
     public
     func getChatView() -> UIViewController? {
         return chatView
     }
-    
-    public
-    func getChatSwiftUIView() -> (any View)? {
-        return chatSwiftUIView
-    }
-    
 }
