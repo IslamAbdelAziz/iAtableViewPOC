fd28165628528f06fca38565e226e0d0ed0337fd
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2023-12-27 03:58:21 +0200

    [+] feat: adding some helping vars

--- Sources/iAtableViewPOC/iAtableViewPOC.swift
+++ Sources/iAtableViewPOC/iAtableViewPOC.swift
@@ -7,6 +7,10 @@ class IAChatManager {
     
     public
     init(chatTableViewCellNib: UINib, identifier: String) {
+        let bundle = Bundle(for: IAChatVC.self)
+        let id = bundle.bundleIdentifier
+        let bundle2 = Bundle(for: ChatVC2.self)
+        let bundle3 = Bundle(for: IAChatVC.self)
 //        chatView = IAChatVC(nibName: "IAChatVC", bundle: Bundle(identifier: "com.IA.iAtableViewPOC"))
         chatView = UIStoryboard(name: "ChatStoryboard", bundle: nil).instantiateViewController(identifier: "ChatVC2")
         chatView?.configureChatView(chatTableViewCellNib: chatTableViewCellNib, identifier: identifier)
